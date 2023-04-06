9caafb782d1a4741496ada53c35072c0ed0f160c
durru <durrutxua@baturamobile.com>
2023-04-07 01:08:03 +0200

    feat: Get airports with scrapi

--- scrapi/app/search/service/sources/euskadi.py
+++ scrapi/app/search/service/sources/euskadi.py
@@ -3,16 +3,36 @@ import json
 from .utils.chromedriver_generator import create_chromedriver
 from seleniumwire.utils import decode
 from __init__ import app
+import bs4 as BeautifulSoup
+
   
 def scrape(url):
     driver = create_chromedriver()
     driver.get(url)
-    app.logger.debug('Euskadi page loaded successfully')
-    driver.requests.reverse()
-    for request in driver.requests:
-        if 'https://api.euskadi.eus/directory/entities' in request.url:
-            app.logger.debug('Euskadi api request found')
-            body = request.response.body.decode('utf-8')
-            json_data = json.loads(body)
-            
-            return body
+    
+    page_source = driver.page_source
+
+    # Pasar el HTML a Beautiful Soup
+    soup = BeautifulSoup(page_source, "html.parser")
+
+    # Crear un archivo CSV y agregar encabezados de columnas
+    output_file = "airport.csv"
+    with open(output_file, "w") as file:
+        file.write("iata,name,country\n")
+
+        # Analizar el HTML y extraer la información
+        for element in soup.find_all("tbody"):
+            for element1 in element.find_all("tr"):
+                info = element1.find_all("td")
+                if len(info) == 3:
+                    iata = info[2].get_text()
+                    name = info[0].get_text()
+                    country = info[1].get_text()
+
+                    # Agregar una nueva línea con los datos al archivo CSV
+                    file.write(f"{iata},{name},{country}\n")
+
+    print(f"Datos guardados en {output_file}.")
+
+    # Cerrar el navegador y finalizar la instancia de ChromeDriver
+    driver.quit()
