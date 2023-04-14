fc08089793902b38ae647e8d67260eb1dd7488d8
durru <durrutxua@baturamobile.com>
2023-04-14 16:25:06 +0200

    Feat: 1: create airline table

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,7 @@
 -- Crear un nuevo usuario y otorgarle privilegios de SELECT en la base de datos db_easyflight
 CREATE USER 'java_user'@'192.168.0.10' IDENTIFIED BY 'fl1nt4c03';
-GRANT SELECT ON db_easyflight.* TO 'java_user'@'192.168.0.10';
+GRANT SELECT, INSERT ON db_easyflight.* TO 'java_user'@'192.168.0.10';
+
 FLUSH PRIVILEGES;
 
 -- Crea la tabla airports
@@ -8,6 +9,12 @@ CREATE TABLE IF NOT EXISTS airport (
     iata VARCHAR(3) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     country VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS airline (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    logo_url VARCHAR(500) NOT NULL
 );
 
 -- Importa los datos del archivo CSV en la tabla airports
